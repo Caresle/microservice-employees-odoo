c9f5cc19b6acdaee231a0a9f5d014038d2e20439
caresle <carabesle@gmail.com>
2024-04-17 20:45:03 -0600

    Add config of odbc of postgresql

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import { config } from "dotenv";
+config();
 import express from "express";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+	return res.json({ data: "Employees service" }).end();
+});
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
---
+++ src/db/odbc/postgresql.ts
@@ -0,0 +1,22 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+	user: process.env.PGUSER,
+	host: process.env.PGHOST,
+	password: process.env.PGPASSWORD,
+	database: process.env.PGDATABASE,
+	port: parseInt(process.env.PGPORT ?? "0"),
+});
+
+export const postgrsql = async (query: string, params: Array<any> = []) => {
+	try {
+		const res = await pool.query(query, params);
+		return res.rows;
+	} catch (error) {
+		console.log(error);
+	} finally {
+		await pool.end();
+	}
+};
